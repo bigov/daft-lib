de6ccf340907e78f390fb84677ca059c5db2985e
bigov <balezin@yandex.ru>
2018-03-24 19:51:49 +0900

    fix

--- ref_to_class_method/main.cpp
+++ ref_to_class_method/main.cpp
@@ -4,35 +4,34 @@ int iVar = 0;
 
 class TestCall
 {
-	public:
-		TestCall() {};
-		static void dri(int);
+  public:
+    TestCall() {};
+    static void dri(int);
 };
 
 void TestCall::dri(int m)
 {
-	std::cout << "Method \"TestCall::dri()\" get arg = " << m << "\n";
-	return;
+  std::cout << "Method \"TestCall::dri()\" get arg = " << m << "\n";
+  return;
 }
 
 ////
 void caller(void(fn_call)(int))
 {
-	fn_call(iVar);
-	return;
+  fn_call(iVar);
+  return;
 };
 
 ////
 int main()
 {
-	std::cout << '\n';
+  std::cout << '\n';
 
-	iVar = 77;
-	TestCall T = {};
-	caller(T.dri);
-	
-	std::cout << " Готово.\n";
-	
+  iVar = 77;
+  TestCall T = {};
+  caller(T.dri);
 
-	return EXIT_SUCCESS;
+  std::cout << " Готово.\n";
+
+  return EXIT_SUCCESS;
 }
