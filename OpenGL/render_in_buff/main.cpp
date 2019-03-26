e7bb2685c84cb7ff2acdecab9145b179e81dca17
Igor Balezin <balezin@yandex.ru>
2019-03-26 18:39:02 +0900

    append render in the framebuffer

--- OpenGL/render_in_buff/main.cpp
+++ OpenGL/render_in_buff/main.cpp
@@ -10,14 +10,26 @@
 
 #define ERR throw std::runtime_error
 
-static GLuint
-  vao_id = 0,
-  data_buf = 0,
-  index_buf = 0,
-  program = 0,
-  attr_position = 0,
-  attr_color = 0;
+int win_w = 640, win_h = 480;
 
+static GLuint
+  frame_buff_id = 0,
+  vao_main = 0,
+  vao_frame = 0,
+  main_program = 0,
+  frame_program = 0,
+  attr_main_position = 0,
+  attr_main_color = 0,
+  attr_buff_position = 0,
+  attr_buff_texcoord = 0,
+  attr_buff_texture = 0,
+  buff_texture = 0;         // текстура рендера пространства
+
+static std::string
+  main_vert_glsl {"glsl/main_vert.glsl"},
+  main_frag_glsl {"glsl/main_frag.glsl"},
+  buff_vert_glsl {"glsl/buff_vert.glsl"},
+  buff_frag_glsl {"glsl/buff_frag.glsl"};
 
 //## File read
 std::unique_ptr<char[]> read_file(const std::string &FNname)
@@ -67,7 +79,7 @@ GLFWwindow* glfw_win(void)
   //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  auto win_ptr = glfwCreateWindow(640, 480, "test", nullptr, nullptr);
+  auto win_ptr = glfwCreateWindow(win_w, win_h, "test", nullptr, nullptr);
   if (nullptr == win_ptr) ERR("Creating Window fail\n");
   glfwSetKeyCallback(win_ptr, key_callback);
   glfwMakeContextCurrent(win_ptr);
@@ -110,21 +122,31 @@ GLuint gl_get_attrib(GLuint program, const std::string& attrib_name)
 }
 
 
-//## OpenGL
-void create_program(void)
+//##
+GLuint gl_get_uniform(GLuint program, const std::string& name )
 {
+  GLint l = glGetUniformLocation(program, name.c_str());
+  if(l < 0) ERR ("ERROR: Not found uniform name: " + name);
+  return static_cast<GLuint>(l);
+}
+
+
+//## OpenGL
+GLuint create_program(const std::string& vert_shader, const std::string& frag_shader)
+{
+  GLuint program = 0;
   GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
   if (!vertShader) ERR("Error create GL_VERTEX_SHADER");
 
   GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
   if (!fragShader) ERR("Error create GL_FRAGMENT_SHADER");
 
-  auto Buff = read_file("glsl/vert.glsl");
+  auto Buff = read_file(vert_shader);
   auto b = Buff.get();
   glShaderSource(vertShader, 1, &b, nullptr);
   compile_shader(vertShader);
 
-  Buff = read_file("glsl/frag.glsl");
+  Buff = read_file(frag_shader);
   b = Buff.get();
   glShaderSource(fragShader, 1, &b, nullptr);
   compile_shader(fragShader);
@@ -151,9 +173,31 @@ void create_program(void)
     }
     ERR("Failed to link GLSL program.\n");
   }
-  glUseProgram(program);
-  attr_position = gl_get_attrib(program, "VertexPosition");
-  attr_color = gl_get_attrib(program, "VertexColor");
+  return program;
+}
+
+
+//##
+void init_pogram_main()
+{
+  main_program = create_program(main_vert_glsl, main_frag_glsl);
+  glUseProgram(main_program);
+  attr_main_position = gl_get_attrib(main_program, "VertexPosition");
+  attr_main_color = gl_get_attrib(main_program, "VertexColor");
+  glUseProgram(0);
+}
+
+
+//##
+void init_pogram_buff()
+{
+  frame_program = create_program(buff_vert_glsl, buff_frag_glsl);
+  glUseProgram(frame_program);
+  attr_buff_position = gl_get_attrib(frame_program, "VertexPosition");
+  attr_buff_texcoord = gl_get_attrib(frame_program, "TextureCoord");
+  attr_buff_texture = gl_get_uniform(frame_program, "texFramebuffer");
+  glActiveTexture(GL_TEXTURE1);
+  glUniform1i(attr_buff_texture, 1); // GL_TEXTURE1 == 1
   glUseProgram(0);
 }
 
@@ -163,59 +207,97 @@ void init_scene(void)
   float pos_and_color[18] = { -0.8f, -0.8f, 0.0f, 0.4f, 1.0f, 0.4f,
                                0.8f, -0.8f, 0.0f, 1.0f, 0.4f, 0.4f,
                                0.0f,  0.8f, 0.0f, 1.0f, 1.0f, 0.4f };
-  GLuint index_data[3] = {0, 1, 2}; // index data array
-
   GLsizei stride = 6 * sizeof(GLfloat);
   GLsizei data_size = 3 * stride;
 
-  glClearColor(0.5f, 0.69f, 1.0f, 1.0f);
-  glGenVertexArrays(1, &vao_id);
-  glBindVertexArray(vao_id);
+  glGenVertexArrays(1, &vao_main);
+  glBindVertexArray(vao_main);
 
   //data buffer
-  glGenBuffers(1, &data_buf);
-  glBindBuffer(GL_ARRAY_BUFFER, data_buf);
+  GLuint _vbo = 0;
+  glGenBuffers(1, &_vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, _vbo);
   glBufferData(GL_ARRAY_BUFFER, data_size, pos_and_color, GL_STATIC_DRAW);
-  glVertexAttribPointer(attr_position, 3, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(0*sizeof(GLfloat)));
-  glVertexAttribPointer(attr_color, 3, GL_FLOAT, GL_TRUE, stride, reinterpret_cast<void*>(3*sizeof(GLfloat)));
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-  //index buffer
-  glGenBuffers(1, &index_buf);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buf);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(float), index_data, GL_STATIC_DRAW);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-  glBindVertexArray(0);
+  glEnableVertexAttribArray(attr_main_position);
+  glEnableVertexAttribArray(attr_main_color);
+  glVertexAttribPointer(attr_main_position, 3, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(0*sizeof(GLfloat)));
+  glVertexAttribPointer(attr_main_color, 3, GL_FLOAT, GL_TRUE, stride, reinterpret_cast<void*>(3*sizeof(GLfloat)));
   return;
+}
+
+//##
+bool init_framebuffer(GLsizei w, GLsizei h)
+{
+  GLint lod = 0, frame = 0;
+  GLfloat Position[8] = {-1.f,-1.f, 1.f,-1.f,-1.f, 1.f, 1.f, 1.f }; // 2D коорд. фрейма
+  GLfloat Texcoord[8] = { 0.f, 0.f, 1.f, 0.f, 0.f, 1.f, 1.f, 1.f }; // UV координаты
+
+  glGenVertexArrays(1, &vao_frame);
+  glBindVertexArray(vao_frame);
+
+  // Буфер координат вершин фрейма, заполняющего окно приложения
+  GLuint vbo_buff_pos = 0;
+  glGenBuffers(1, &vbo_buff_pos);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_buff_pos);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Position), Position, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(attr_buff_position);
+  glVertexAttribPointer(attr_buff_position, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+  // Буфер UV координат текстуры, в которую рендерится сцена
+  GLuint vbo_buff_tex = 0;
+  glGenBuffers(1, &vbo_buff_tex);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_buff_tex);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Texcoord), Texcoord, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(attr_buff_texcoord);
+  glVertexAttribPointer(attr_buff_texcoord, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+  // Память под текстуру, в которую рендерится основная сцена
+  glGenTextures(1, &buff_texture);
+  glBindTexture(GL_TEXTURE_2D, buff_texture);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexImage2D(GL_TEXTURE_2D, lod, GL_RGBA, w, h, frame, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+
+  glGenFramebuffers(1, &frame_buff_id);
+  glBindFramebuffer(GL_FRAMEBUFFER, frame_buff_id);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, buff_texture, 0);
+
+  //GLenum b[1] = { GL_COLOR_ATTACHMENT0 }; //, GL_COLOR_ATTACHMENT1 };
+  //glDrawBuffers(1, b);
+
+  GLuint rbuf = 0;
+  glGenRenderbuffers(1, &rbuf);
+  glBindRenderbuffer(GL_RENDERBUFFER, rbuf);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbuf);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, w, h);
+
+  return glGetError() == GL_NO_ERROR;
 }
 
 //## ---
 void show(GLFWwindow* win_ptr)
 {
-  create_program();
-  init_scene();
+  glClearColor(0.5f, 0.69f, 1.0f, 1.0f);
+  glDisable(GL_DEPTH_TEST);
 
+  init_pogram_main();
+  init_pogram_buff();
+  init_scene();
+  init_framebuffer(win_w, win_h);
+
+  glBindFramebuffer(GL_FRAMEBUFFER, frame_buff_id);
+  glBindVertexArray(vao_main);
+  glUseProgram(main_program);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+  glBindVertexArray(vao_frame);
+  glUseProgram(frame_program);
+  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+  glfwSwapBuffers(win_ptr);
   while (!glfwWindowShouldClose(win_ptr))
   {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glBindVertexArray(vao_id);
-    glBindBuffer(GL_ARRAY_BUFFER, data_buf);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buf);
-    glEnableVertexAttribArray(attr_position);
-    glEnableVertexAttribArray(attr_color);
-
-    glUseProgram(program);
-    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
-    //glDrawElementsBaseVertex(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr, 0);
-
-    glDisableVertexAttribArray(attr_color);
-    glDisableVertexAttribArray(attr_position);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-    glfwSwapBuffers(win_ptr);
     glfwPollEvents();
     std::this_thread::sleep_for(std::chrono::milliseconds(20));
   }
