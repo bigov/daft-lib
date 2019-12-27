a8dfe2e88147747b328f532567bdc7e4e51a434f
U-MNV\igor.balezin <bigov@yandex.ru>
2019-12-27 16:50:54 +0900

    some fix

--- mix/c_strings/2tst.c
+++ mix/c_strings/2tst.c
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <time.h>
-
-char ErrStr[0]; 	// Текс сообщения об ошибке
-
-// Функция прерывания программы из библиотеки stdlib.h
-void exit(int);
-
-/**
- * Возвращает 1 (true), если есть сообщения об ошибках
- */
-bool NoErrors(void)
-{
-  if (strlen(ErrStr)) return false;
-  else return true;
-}
-/**
- * Очистка строки ошибок
- */
-void ClearErrors(void)
-{
-  memset(ErrStr, '\0', strlen(ErrStr));
-  return;
-}
-
-int main(int argc, char* argv[])
-{
-    // отображение текущего времени в системе
-    const time_t timer = time(NULL);
-    printf("%s\n", ctime(&timer));
-
-    strcat(ErrStr, "First message.");
-    strcat(ErrStr, "\nMy second message.");
-    fprintf(stderr, "Std Err: %s", ErrStr);
-    strcat(ErrStr, " Final message.");
-
-    printf("\n\nErrStr: %s\nErrLen: %i\n\n", ErrStr, strlen(ErrStr));
-
-    ClearErrors();
-    strcat(ErrStr, "New message");
-    printf("ErrStr: %s\nErrLen: %i\n\n", ErrStr, strlen(ErrStr));
-    if(NoErrors()) printf("No error messages\n");
-    
-    float t = clock();
-    fprintf(stdout, "job time: %f(sec)\n", t/CLOCKS_PER_SEC);
-
-    return EXIT_SUCCESS;
-}
-=======
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -102,4 +49,3 @@ int main(int argc, char* argv[])
 
     return EXIT_SUCCESS;
 }
->>>>>>> ba964a66419c63ab368d123aa3f2a9a170435aa6
