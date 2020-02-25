755f13a3989e534895b3cf1ef0bc02feef17babd
bigov <bigov@yandex.com>
2020-02-25 17:00:56 +0900

    add crono sample

--- mix/tst_std_crono.cpp
+++ mix/tst_std_crono.cpp
@@ -3,16 +3,16 @@
 
 int main()
 {
-	std::chrono::milliseconds ms = std::chrono::duration_cast< std::chrono::milliseconds >
+	auto ms = std::chrono::duration_cast< std::chrono::milliseconds >
 		(std::chrono::system_clock::now().time_since_epoch());
 
-	// миллисекунд в сутках
-	int k = 1000 * 60 * 60 * 24;
-	long long int f_now = ms.count();
-	// число миллисекунд от начала суток
-	int ms_res = ms.count() % k;
+	auto sec = std::chrono::duration_cast< std::chrono::seconds >
+		(std::chrono::system_clock::now().time_since_epoch());
 
-	std::cout << "ms_res=" << ms_res << "\n";
+	std::cout
+    << "ms="    << ms.count()
+    << "\nsec=" << sec.count()
+    << "\n";
 
 	return 0;
 }
